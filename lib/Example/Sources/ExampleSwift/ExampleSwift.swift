e53c5e593fdb8eedb8ae0460b0e65316b348b3ba
Schubert Anselme <schubert@anselm.es>
2025-12-28 11:51:50 -0500

    Adds initial Swift example implementation
    Creates a basic example function that demonstrates Swift integration with the existing system library.
    
    Includes placeholder code for Foundation-based system information functionality that can be enabled when needed.
    
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

---
+++ lib/Example/Sources/ExampleSwift/ExampleSwift.swift
@@ -0,0 +1,13 @@
+import ExampleSys
+
+public func run() {
+  print("example - Hello from Swift!")
+
+  example()
+
+
+  // #if canImport(Foundation)
+  // let sys = ExampleStruct()
+  // print(sys.systemInfo())
+  // #endif
+}
